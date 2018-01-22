7b7c02cae7a95d7831e16b7140e7a6917000bc89
chenjin <chenjin@coocaa.com>
2018-01-22 14:41:30 +0800

    提交代码。。。。。。。。。。。。。。 1、

--- src/main/java/org/cboard/util/Constans.java
+++ src/main/java/org/cboard/util/Constans.java
@@ -7,5 +7,4 @@ public class Constans {
     public static String SQL_DAY = "date_sub(curdate(), interval {day_num} day)";
     public static String SQL_GTEQ = ">=";
     public static String DAY_CN="日期";
-
 }
